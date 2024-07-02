6302e6f2a52c23fb255e03e39cabcad091b1e292
ELGOKU23 <120664905+ELGOKU23@users.noreply.github.com>
2024-07-02 01:53:30 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y graphviz
+
+# Instalar Conda
+if [ ! -d "$HOME/miniconda3" ]; then
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+  bash miniconda.sh -b -p $HOME/miniconda3
+  export PATH="$HOME/miniconda3/bin:$PATH"
+  conda init bash
+fi
+
+# Cargar Conda
+source $HOME/miniconda3/etc/profile.d/conda.sh
+
+# Crear un entorno Conda y activar
+conda create -n myenv python=3.8 -y
+conda activate myenv
+
+# Instalar dependencias
+conda install -c conda-forge graphviz python-graphviz -y
+
+# Instalar otras dependencias con pip
+pip install -r requirements.txt
